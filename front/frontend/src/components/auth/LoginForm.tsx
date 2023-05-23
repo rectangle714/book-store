98c94e74c2fdf02025387716287a7a7ae5189ba8
rectangle714@naver.com <rectangle714@naver.com>
2023-05-23 17:26:16 +0900

    메인, 로그인, 회원가입 화면 개발 진행중 20230523

---
+++ front/frontend/src/components/auth/LoginForm.tsx
@@ -0,0 +1,56 @@
+import { useState, useRef, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import AuthContext from "../../store/auth-context";
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Classes from '../../styles/LoginForm.module.css';
+
+const LoginForm = () => {
+    const emailInputRef = useRef<HTMLInputElement>(null);
+    const passwordInputRef = useRef<HTMLInputElement>(null);
+
+    let navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(false);
+    const authCtx = useContext(AuthContext);
+
+    const submitHandler = async(evnet: React.FormEvent) => {
+        const enteredEmail = emailInputRef.current!.value;
+        const enteredPasswod = passwordInputRef.current!.value;
+
+        setIsLoading(true);
+        authCtx.login(enteredEmail, enteredPasswod);
+        setIsLoading(false);
+
+        if(authCtx.isSuccess) {
+            navigate("/", {replace: true});
+        }
+
+    }
+
+    return (
+        <section className={Classes.loginSection}>
+            <h1>로그인</h1>
+            <form onSubmit={submitHandler}>
+                <div>
+                    <TextField label="이메일" variant="standard" id='email' required inputRef={emailInputRef} />
+                </div>
+                <div>
+                    <TextField label="패스워드" type="password" variant="standard" id='password' required inputRef={passwordInputRef} />
+                </div>
+                <div style={{
+                    paddingTop: 30
+                }}>
+                <ButtonGroup variant="outlined" aria-label="outlined button group">
+                    <Button type='submit'>로그인</Button>
+                    {isLoading && <p>Loading</p>}
+                    <Button>계정 생성</Button>
+                </ButtonGroup>
+                </div>
+            </form>
+        </section>
+    )
+
+}
+
+export default LoginForm;
---
+++ front/frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,7 @@
+import AuthForm from "../components/auth/LoginForm";
+
+const AuthPage = () => {
+    return <AuthForm />;
+}
+
+export default AuthPage;
