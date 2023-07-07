655ddde82c478b74634d322262a3f8b9dca1028f
rectangle714@naver.com <rectangle714@naver.com>
2023-07-07 17:10:43 +0900

    메인페이지 slider 추가 20230707

--- front/frontend/src/components/layout/MainPage.tsx
+++ front/frontend/src/components/layout/MainPage.tsx
@@ -1,84 +1,13 @@
-import React, { useEffect } from 'react';
 import Container from '@mui/material/Container';
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
-import { Tabs, Tab, Box, Typography } from '@mui/material';
 import { GET } from "../../store/fetch-auth-action";
 import Styls from'./MainPage.module.css'
-
-interface TabPanelProps {
-    children?: React.ReactNode;
-    index: number;
-    value: number;
-  }
-
-function a11yProps(index: number) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
-}
-
-function TabPanel(props: TabPanelProps) {
-    const { children, value, index, ...other } = props;
-  
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box sx={{ p: 3 }}>
-            {children}
-          </Box>
-        )}
-      </div>
-    );
-  }
+import Slider from './Slider';
 
 const MainPage = () => {
 
-    const [value, setValue] = React.useState(0);
-    let [rows, setRows] = React.useState([]);
-
-    useEffect(() => {
-        rowData();
-    },[]);
-
-    const columns: GridColDef[] = [
-        { field: 'id', headerName: 'ID', width: 70 },
-        { field: 'email', headerName: '이메일', width: 130 },
-        { field: 'nickname', headerName: '닉네임', width: 130 },
-        {
-          field: 'authority',
-          headerName: '권한',
-          description: 'This column has a value getter and is not sortable.',
-          sortable: false,
-          width: 160,
-          valueGetter: ({value}) => value=='ROLE_USER'? '사용자':'관리자',
-        },
-      ];
-
-    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-        setValue(newValue);
-        rowData();
-    };
-
-    const rowData = () => {
-        const URL = '/member/findAll'
-        let response = GET(URL, {});
-        response.then((result) => {
-            if(result != null) {
-                setRows(result.data);
-            }
-        });
-    }
-
     return (
         <Container maxWidth="md" fixed className={Styls.container}>
-            슬라이더 자리
+          <Slider/>
         </Container>
     )
 }
---
+++ front/frontend/src/components/layout/Slider.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import imageData from "../../store/imageData";
+import Styles from "./Slider.module.css";
+
+const Slider = () => {
+
+    const renderSlides = imageData.map(image => (
+        <div key={image.alt} style={{width: '200px', margin: 'auto'}}>
+          <img src={image.url} alt={image.alt} className={Styles.carousel} />
+        </div>
+    ));
+    
+    const [currentIndex, setCurrentIndex] = useState(0);
+    
+    function handleChange(index:number) {
+      setCurrentIndex(index);
+    }
+
+    return (
+        <div style={{paddingTop:'20px'}}>
+            <Carousel
+            showArrows={false}
+            autoPlay={true}
+            infiniteLoop={true}
+            showThumbs={false}
+            onChange={handleChange}>
+            {renderSlides}
+            </Carousel>
+        </div>
+    )
+}
+
+export default Slider;
