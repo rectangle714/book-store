39cc5678b2b877cde6b4a1a84f49992b7615e482
rectangle714@naver.com <rectangle714@naver.com>
2023-11-08 15:33:29 +0900

    NaverToken 추가 20231108

---
+++ back/bootProject/src/main/java/com/bootProject/dto/NaverToken.java
@@ -0,0 +1,10 @@
+package com.bootProject.dto;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+public class NaverToken {
+    private String aceessToken;
+    private String tokenType;
+}
