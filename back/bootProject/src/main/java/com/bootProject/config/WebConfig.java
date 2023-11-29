48671a95ab3e98a65f7d80eee6786d85d61089d0
rectangle714@naver.com <rectangle714@naver.com>
2023-11-30 08:04:32 +0900

    배포서버 Cors 해결 진행중 20231130

--- back/bootProject/src/main/java/com/bootProject/config/WebConfig.java
+++ back/bootProject/src/main/java/com/bootProject/config/WebConfig.java
@@ -1,8 +1,12 @@
 package com.bootProject.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -10,6 +14,8 @@ public class WebConfig implements WebMvcConfigurer {
                 .exposedHeaders("Authorization")
                 .exposedHeaders("RefreshToken")
                 .allowCredentials(true)
-                .allowedOrigins("http://54.168.229.128:3000/","http://localhost:3000");
+                .allowedOrigins("http://54.168.229.128:3000","http://localhost:3000")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
+
     }
 }
