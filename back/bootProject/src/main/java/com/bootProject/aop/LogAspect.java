479025af18535559f547a7d953684766b3c78ab5
rectangle714@naver.com <rectangle714@naver.com>
2023-12-05 10:08:38 +0900

    aop 적용 20231205

---
+++ back/bootProject/src/main/java/com/bootProject/aop/LogAspect.java
@@ -0,0 +1,37 @@
+package com.bootProject.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Aspect
+@Component
+@Slf4j
+public class LogAspect {
+
+    @Around("execution(public * com.bootProject.controller.*.*(..))")
+    public Object logRequestTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        // @Before 수행
+        log.info("{} 요청 시작 - {}", joinPoint.getSignature(),LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss:SSS")));
+        // @Before 종료
+
+        // Target 메서드 호출
+        Object result = joinPoint.proceed();
+        // Target 메서드 종료
+
+        // @AfterReturning 수행
+        log.info("{} 요청 완료 - {}", joinPoint.getSignature(),LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss:SSS")));
+        // @AfterReturning 종료
+        return result;
+    }
+
+}
