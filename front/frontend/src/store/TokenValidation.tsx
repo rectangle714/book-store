87eb317255cba6435171afd391e9fdbae368434d
rectangle714@naver.com <rectangle714@naver.com>
2023-12-19 10:39:54 +0900

    TokenValidation cookie 값 에러 수정 20231219v2

--- front/frontend/src/store/TokenValidation.tsx
+++ front/frontend/src/store/TokenValidation.tsx
@@ -3,28 +3,22 @@ import { useEffect } from 'react';
 import { getCookie } from './cookie';
 
 const TokenValidation = () => {
-  useEffect(() => {
-    const accessToken = getCookie('accessToken');
-    const refreshToken = getCookie('refreshToken');
-    
-    const interceptor = axios.interceptors.request.use(
+    axios.interceptors.request.use(
       function (request) {
+        const accessToken = getCookie('accessToken');
+        const refreshToken = getCookie('refreshToken');
         if( undefined != accessToken && undefined != refreshToken ) {
-          request.headers.Authorization = 'Bearer ' + accessToken;
+          request.headers.Authorization = 'Bearer ' + accessToken; 
           request.headers.RefreshToken = 'Bearer ' + refreshToken;
           request.withCredentials = true;
-          console.log('request ',request);
-          console.log('요청 성공');
         }
         return request;
       },
       async function (error) {
-
         console.log('요청 실패');
         return Promise.reject(error);
       }
     );
-  }, []);
   return (<></>);
 };
 
