b05a82347e3d8927b3d609de567eac97aa832963
rectangle714@naver.com <rectangle714@naver.com>
2023-12-23 12:58:47 +0900

    index ts 파일 추가 커밋 20231223

---
+++ front/frontend/src/pages/index.ts
@@ -0,0 +1,10 @@
+import AdminPage from "./AdminPage"; 
+import CreateAccountPage from "./CreateAccountPage";
+import FindEmailPage from "./FindEmailPage";
+import FindPasswordPage from "./FindPasswordPage";
+import HomePage from "./HomePage";
+import LoginPage from "./LoginPage";
+import ProfilePage from "./ProfilePage";
+import UpdatePasswordPage from "./UpdatePasswordPage";
+
+export {AdminPage, CreateAccountPage, FindEmailPage, FindPasswordPage, HomePage, LoginPage, ProfilePage, UpdatePasswordPage};
