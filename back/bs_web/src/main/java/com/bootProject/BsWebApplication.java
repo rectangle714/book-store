489bf6f97585f81be1b9e9bec6369f598c2f1aff
rectangle714@naver.com <rectangle714@naver.com>
2024-02-21 09:28:58 +0900

    프로젝트명 변경 - 20240221

--- back/bs_web/src/main/java/com/bootProject/BsWebApplication.java
+++ back/bs_web/src/main/java/com/bootProject/BsWebApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootProjectApplication {
+public class BsWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootProjectApplication.class, args);
+		SpringApplication.run(BsWebApplication.class, args);
 	}
 
 }
